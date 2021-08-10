08c00849c7e0a47d883be3062a5cdd49316b293c
Jeff T <jeff.thao92@gmail.com>
2021-08-10 00:07:16 -0500

    Added local up description into jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import { Jumbotron, Button, Container } from 'react-bootstrap';
-
+import React from "react";
+import { Jumbotron, Button, Container } from "react-bootstrap";
 
 function Jumbo() {
-return (
-	<>
-	<Jumbotron>
-	<h1>Local UP!</h1>
-	<p>
-		This is a simple Jumbotron example.
-	</p>
+  return (
+      <>
+    <Jumbotron> 
+      <h1>Local UP!</h1>
+      <p>
+        Our organization helps small and local businesses with marketing, search
+        engine optimization (SEO), and technical services such as setting up a
+        website. The goal of our organization is to give small local businesses
+        a ost-effective way to market their services. Our overall mission is to
+        give local businesses low-cost marketing olutions. As well as establish
+        them an e-commerce presence by creating them website where they can sell
+        their goods online.
+      </p>
 
-	<p>
-		<Button variant="primary">
-		Primary Button
-		</Button>
-	</p>
-	</Jumbotron>
-	<br/>
-	<Jumbotron fluid>
-	<Container>
-		<h1>Fluid jumbotron !</h1>
-		<p>
-		This is a modified fluid jumbotron which
-		stretches the whole horizontal space.	
-		</p>
-		<Button variant="primary">
-		Primary Button
-		</Button>
-	</Container>
-	</Jumbotron>
-	</>
-);
+      <p>
+        <Button variant="primary">Primary Button</Button>
+      </p>
+    </Jumbotron>
+    <br/>
+    <Jumbotron fluid>
+      <Container>
+        <h1>Fluid jumbotron !</h1>
+        <p>
+           This is a modified fluid jumbotron which
+           stretches the whole horizontal space.    
+        </p>
+        <Button variant="primary">
+         Primary Button
+        </Button>
+      </Container>
+    </Jumbotron>
+    </>
+  );
 }
-export default Jumbo
+export default Jumbo;
